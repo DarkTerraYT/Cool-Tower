e2f001979dc7efeea6ce9f045b3dab467d0aeae1
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2023-10-30 22:03:29 -0400

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace TrollTowerMod;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "38.3";
+    public const string Version = "1.0.0";
+    public const string Name = "???";
+
+    public const string Description = "Adds a cool ??? tower to the game :)";
+
+    public const string RepoOwner = "Mattcy / DarkTerra"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "???"; // TODO add your repo name here, also in the download url in README.md
+}
