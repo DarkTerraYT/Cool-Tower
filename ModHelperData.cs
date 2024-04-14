1c8c74200318c6d53097cd94e21bb27c124dd2da
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-04-14 12:11:05 -0400

    fix for v42.1

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,14 +2,12 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "38.3";
-    public const string Version = "1.0.1";
+    public const string WorksOnVersion = "42.1";
+    public const string Version = "1.0.2";
     public const string Name = "???";
 
-    public const string Description = "Adds a cool ??? tower to the game :) also made by Mattcy1 (discord: @lerpmcgerk)";
+    public const string Description = "Adds a cool ??? tower to the game :) also made by Mattcy1";
 
     public const string RepoOwner = "Mattcy / DarkTerra"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "???"; // TODO add your repo name here, also in the download url in README.md
-
-    internal const string Author = "Mattcy1 and LerpMcGerk"
 }
