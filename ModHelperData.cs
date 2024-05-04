3d7773d5bebf99cc288770f671255dc4e15c79f9
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-05-04 12:44:02 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,5 +9,6 @@ public static class ModHelperData
     public const string Description = "Adds a cool ??? tower to the game :) also made by Mattcy1";
 
     public const string RepoOwner = "Mattcy / DarkTerra"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "???"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "Cool-Tower"; // TODO add your repo name here, also in the download url in README.md
+    public const string Author = "Mattcy1 / DarkTerraYT";
 }
