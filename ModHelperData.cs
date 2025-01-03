32f51d62082b4b8b62e772b4b2f39b342f372c4c
Liam <105320711+DarkTerraYT@users.noreply.github.com>
2025-01-03 15:32:47 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "42.1";
-    public const string Version = "1.0.2";
+    public const string WorksOnVersion = "46.3";
+    public const string Version = "1.0.3";
     public const string Name = "???";
 
     public const string Description = "Adds a cool ??? tower to the game :) also made by Mattcy1";
